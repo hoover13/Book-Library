7f8b34165c25e07b5e3b03e3bee8c3d2da9b79f5
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-28 02:57:09 -0400

    Initial Commit

---
+++ Book Library/Book_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_LibraryApp.swift
+//  Book Library
+//
+//  Created by Hoover on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Book_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
